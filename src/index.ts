14244a39b8b4ed17ff614875d1b83ce9d69b2d42
razvancanuci <99394985+razvancanuci@users.noreply.github.com>
2025-08-09 18:22:04 +0300

    Add message for starting the server

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {configDotenv} from "dotenv";
 import {registerResources} from "./resources/index.js";
 import {registerPrompts} from "./prompts/index.js";
 import apm from 'elastic-apm-node';
+import {logger} from "./infrastructure/logger.js";
 
 configDotenv();
 // Create an MCP server
@@ -32,6 +33,9 @@ apm.start({
 
     instrument: true,
 });
+
+logger.info('Starting MCP server...');
+
 const transport = new StdioServerTransport();
 await server.connect(transport);
 
